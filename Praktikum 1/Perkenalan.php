5ab50a30527e3785cfdcef09dca3c9b2b13fb44b
dimdim <dimas.gunarso@gmail.com>
2020-10-20 12:38:08 +0700

    first commit

---
+++ Praktikum 1/Perkenalan.php
@@ -0,0 +1,13 @@
+<?php
+
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Dimas Shofa Gunarso<br>
+        Saya tingga di daerah Pemalang<br>
+        olah raga yang saya sukai adalah
+        Bulutangkis";
+    }
+}
